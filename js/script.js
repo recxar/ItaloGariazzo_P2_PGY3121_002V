bb9ac0199a74b6c9387350a1c7fc0673470afa43
recxar <it.gariazzo@duocuc.cl>
2022-06-02 00:54:09 -0400

    xd

--- js/script.js
+++ js/script.js
@@ -5,10 +5,3 @@ function getBasePath(service){
 function createRowUltimosSismosChile(referencia, magnitud, latitud, longitud, hora) {
     return '<tr><td>'+referencia+'</td><td>'+magnitud+'</td><td>'+latitud+'</td><td>'+longitud+'</td><td>'+hora+'</td></tr>';
 }
-function createRowUltimossismos(referencia, magnitud, latitud, longitud, hora) {
-    return '<tr><td>'+referencia+'</td><td>'+magnitud+'</td><td>'+latitud+'</td><td>'+longitud+'</td><td>'+hora+'</td></tr>';
-}
-
-function createRowTsunamichile(referencia, magnitud, latitud, longitud, hora) {
-    return '<tr><td>'+referencia+'</td><td>'+magnitud+'</td><td>'+latitud+'</td><td>'+longitud+'</td><td>'+hora+'</td></tr>';
-}
